1ccc94e1fd043c125181d4b2002ca558cf37ee0f
Saira Shafiq <hafsa.noor652@gmail.com>
2026-06-24 13:32:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function validateForm(){
+
+let name=document.getElementById("name").value;
+let email=document.getElementById("email").value;
+
+if(name==="" || email===""){
+alert("Please fill all fields");
+return false;
+}
+
+alert("Form Submitted Successfully");
+return true;
+}
